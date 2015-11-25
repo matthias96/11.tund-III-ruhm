71714b97fe44cfa1a56f38f54be5c1e098169abc
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2015-11-25 11:45:11 +0200

    woosh

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
-
 <?php require_once("header.php"); ?>
 
 	<!----#####################-->
-	<!-----------MenÃ¼Ã¼----------->
+	<!-----------Menüü----------->
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -23,42 +22,32 @@
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-  
-	<br><br><br><br>
 
-	<!----#####################-->
-	<!-----------SISU----------->
-	
 <div class="container">
-
 	<div class="row">
-		<div class="col-sm-6 col-sm-push-6">
-			<ul class="nav nav-tabs">
-				<li role="presentation" class="active"><a href="#">Home</a></li>
-				<li role="presentation"><a href="#">Profile</a></li>
-				<li role="presentation"><a href="#">Messages</a></li>
-			</ul>
-		</div>	
+		<div class="col-sm-6 col-sm-offset-1">
+			<h1> Tere </h1>
+		</div>
 		
-		<div class="col-sm-6 col-sm-pull-6">
-			<div class="jumbotron">
-				<div class="container">
-			<h1>Hello, world!</h1>
-			<p>...</p>
-			<p><a class="btn btn-success pull-right" href="#" role="button">Learn more</a></p>
-				</div>
-			</div>	
-			
-		</div>	
-		
-		
-		
-	</div>
-	
-</div>	
-	
-	
-	
-	
-<?php require_once("footer.php"); ?>
-   
+		<div class="col-sm-3 col-sm-offset-1">
+	<form>
+		<div class="form-group">
+			<label for="exampleInputEmail1">Email address</label>
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+		</div>
+		<div class="form-group">
+			<label for="exampleInputPassword1">Password</label>
+				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		</div>
+		<div class="form-group">
+			<label for="exampleInputFile">File input</label>
+				<input type="file" id="exampleInputFile">
+		<p class="help-block">Example block-level help text here.</p>
+		</div>
+		<div class="checkbox">
+		<label>
+			<input type="checkbox"> Check me out
+		</label>
+		</div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
